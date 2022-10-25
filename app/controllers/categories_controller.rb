7550f26818d51ddc4ec325c618eda6894837ad1e
Rullian Rong <naillur.gnor@gmail.com>
2022-10-25 10:05:59 +0800

    apply design for headers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -49,7 +49,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = current_user.Category.find(params[:id])
+      @category = current_user.categories.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
