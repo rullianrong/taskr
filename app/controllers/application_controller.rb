8a04dbabcb729715b77ddd4c4ff4e06bb789f221
Rullian Rong <naillur.gnor@gmail.com>
2022-10-27 19:06:27 +0800

    added design to category view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :current_url
+
+    def current_url
+        session[:current_url] = 1
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   end
   
   resources :tasks
-  resources :categories
+  resources :categories do
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
