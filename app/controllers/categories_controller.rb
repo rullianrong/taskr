09efe25d99e5390b2fdd57119c702050bab4e702
Rullian Rong <naillur.gnor@gmail.com>
2022-10-30 15:02:33 +0800

    configure navbar for each page and redirect category#index to authenticated root path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   def index
     @categories = current_user.categories
+    redirect_to :authenticated_root
   end
 
   # GET /categories/1
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, :uniqueness => true
   validates_length_of :title, :maximum => 25
 
   belongs_to :user
